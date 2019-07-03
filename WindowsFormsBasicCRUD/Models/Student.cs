57a0f4fcd0e5d95bebfeb4b70da29d625f818405
Mark Anthony <jazmark2215@gmail.com>
2019-07-03 23:03:03 +0800

    Create Table Mapping

--- WindowsFormsBasicCRUD/Models/Student.cs
+++ WindowsFormsBasicCRUD/Models/Student.cs
@@ -8,5 +8,18 @@ namespace WindowsFormsBasicCRUD.Models
 {
 	public class Student
 	{
+		public string Name { get; set; }
+		public DateTime Birthdate { get; set; }
+		public string Gender { get; set; }
+		public string Address { get; set; }
+		public string School { get; set; }
+		public Status Status { get; set; }
+		public DateTime Recorded { get; set; }
+	}
+
+	public enum Status : byte
+	{
+		Active,
+		Dropped
 	}
 }
