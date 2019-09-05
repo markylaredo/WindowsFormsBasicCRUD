d378f972999e29dd30d14942737cb15b6a1fbaf0
Mark Anthony <markanthonylaredo@gmail.com>
2019-09-05 23:11:56 +0800

    Refactor some code

--- WindowsFormsBasicCRUD/Helpers/Database.cs
+++ WindowsFormsBasicCRUD/Helpers/Database.cs
@@ -6,11 +6,11 @@ namespace WindowsFormsBasicCRUD.Helpers
 {
 	public static class Database
 	{
-		public const string Connection = "Data Source=.;Initial Catalog=TUTStudentDb;Integrated Security=True";
+		private static readonly string _connection = "Data Source=.;Initial Catalog=TUTStudentDb;Integrated Security=True";
 
 		public static void Execute<T>(this T obj, string query)
 		{
-			using (IDbConnection con = new SqlConnection(Database.Connection))
+			using (IDbConnection con = new SqlConnection(_connection))
 			{
 				con.Execute(query, obj);
 			}
